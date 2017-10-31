a16284ed45c4f24a861dddea1e66284ef5d311fe
Jon Sanders <jonsanders101@aol.com>
2017-10-31 12:09:19 +0000

    Add test for host adding multiple spaces

--- spec/features/list_spec.rb
+++ spec/features/list_spec.rb
@@ -5,9 +5,16 @@ feature "When I create a listing" do
       sign_up
     end
 
-    scenario "I can add that listing" do
+    scenario "I can add a listing" do
       expect { post_listing }.to change(Space, :count).by(1)
       expect(current_path).to eq "/all_spaces"
+    end
+
+    scenario "I can add multiple listings" do
+      post_listing
+      sign_up
+      post_listing
+      expect(Space.count).to eq 2
     end
   end
 end
