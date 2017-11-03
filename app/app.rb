8300664c47035362fdc33b081da01e0f57d0a7c5
David Halewood <davidnhalewood@hotmail.com>
2017-11-03 12:56:38 +0000

    Final refactoring of app and server for phone verification logic

--- app/app.rb
+++ app/app.rb
@@ -3,6 +3,7 @@ ENV['RACK_ENV'] ||= 'development'
 require_relative 'data_mapper_setup'
 require 'sinatra/base'
 require 'sinatra/flash'
+require 'sinatra/partial'
 require 'sinatra/multi_route'
 require 'twilio-ruby'
 require 'sanitize'
@@ -10,41 +11,12 @@ require 'erb'
 require 'rotp'
 require 'pry'
 
-include ERB::Util
-require 'sinatra/partial'
-
 require_relative 'server'
 require_relative 'controllers/booking'
 require_relative 'controllers/sessions'
 require_relative 'controllers/spaces'
 require_relative 'controllers/users'
 
-
+include ERB::Util
 include DataMapperSetup
 data_mapper_setup
-
-class MakersBnb < Sinatra::Base
-
-  before do
-    @twilio_number = ENV['TWILIO_NUMBER']
-    @account_number = ENV['ACCOUNT_SID']
-    @auth_token = ENV['AUTH_TOKEN']
-
-    @client = Twilio::REST::Client.new @account_number, @auth_token
-
-    if params[:error].nil?
-      @error =false
-    else
-      @error = false
-    end
-  end
-
-  get '/' do
-    erb :homepage
-  end
-
-  set :partial_template_engine, :erb
-  enable :partial_underscores
-
-  run! if app_file == $0
-end
--- app/server.rb
+++ app/server.rb
@@ -8,6 +8,24 @@ class MakersBnb < Sinatra::Base
   set :partial_template_engine, :erb
   enable :partial_underscores
 
+  before do
+    @twilio_number = ENV['TWILIO_NUMBER']
+    @account_number = ENV['ACCOUNT_SID']
+    @auth_token = ENV['AUTH_TOKEN']
+
+    @client = Twilio::REST::Client.new @account_number, @auth_token
+
+    if params[:error].nil?
+      @error =false
+    else
+      @error = false
+    end
+  end
+
+  get '/' do
+    erb :homepage
+  end
+
   helpers do
     def current_user
       @current_user ||= User.get(session[:user_id])
