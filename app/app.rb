f47c9a0235338e4ba5fee1368bcafb86c6dd411b
David Halewood <davidnhalewood@hotmail.com>
2017-11-03 12:44:38 +0000

    Removing duplicate session logic and routes from app

--- app/app.rb
+++ app/app.rb
@@ -43,26 +43,6 @@ class MakersBnb < Sinatra::Base
     erb :homepage
   end
 
-  post '/sessions' do
-    user = User.authenticate(params[:email], params[:password])
-    if user
-      session[:user_id] = user.id
-      redirect '/'
-    else
-      flash.now[:errors] = ['Woops! Email or password is incorrect.']
-      erb :'sessions/new'
-  end
-end
-
-  get '/sessions/new' do
-    erb :'sessions/new'
-  end
-
-  delete '/sessions' do
-    session[:user_id] = nil
-    redirect '/'
-  end
-
   post '/users' do
     user = User.create(username: params[:username],
                        email: params[:email],
