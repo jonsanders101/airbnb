50574fd4507ab07fca7a86a3279c3c0f3f01f1c8
Jon Sanders <jonsanders101@aol.com>
2017-11-04 09:20:03 +0000

    Progress

--- spec/features/resetting_password_spec.rb
+++ spec/features/resetting_password_spec.rb
@@ -2,6 +2,17 @@ feature 'Resetting password' do
   scenario 'When I forget my password, I can click a link to reset it' do
     visit '/'
     click_link 'forgot-password'
+    expect(page).to have_current_path('/users/recover')
     expect(page).to have_content('Please enter your email address.')
   end
+
+  feature 'Given that I have signed up' do
+    before(:each) sign_up
+  end
+
+  scenario 'I am assigned a reset token' do
+    visit '/'
+    click_link 'forgot-password'
+    within(:css, \//form[@id=])
+    fill_in('email')
 end
